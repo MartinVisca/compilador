975f1db2066e0700550668ced95526958669136f
Martin Ignacio Visca <mvisca@conicet.gov.ar@18-037.devel.conicet.gov.ar>
2020-09-21 18:54:55 -0300

    Se agrega la clase AccionSemanticaSimple.

---
+++ src/accionSemantica/accionSemanticaSimple/AccionSemanticaSimple.java
@@ -0,0 +1,25 @@
+package accionSemantica.accionSemanticaSimple;
+
+import accionSemantica.AccionSemantica;
+import analizadorLexico.AnalizadorLexico;
+
+@SuppressWarnings("all")
+public abstract class AccionSemanticaSimple implements AccionSemantica {
+
+    AnalizadorLexico analizadorLexico;
+
+    public AccionSemanticaSimple() {
+        this.analizadorLexico = new AnalizadorLexico();
+    }
+
+    public AnalizadorLexico getAnalizadorLexico() {
+        return analizadorLexico;
+    }
+
+    public void setAnalizadorLexico(AnalizadorLexico analizadorLexico) {
+        this.analizadorLexico = analizadorLexico;
+    }
+
+    @Override
+    public abstract boolean ejecutar(String buffer, char caracter);
+}

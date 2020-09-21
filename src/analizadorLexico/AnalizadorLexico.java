b4e9f10b3cf442b4cbf034daf21d012220903918
Martin Ignacio Visca <mvisca@conicet.gov.ar@18-037.devel.conicet.gov.ar>
2020-09-21 18:42:33 -0300

    Se crea la clase AnalizadorLexico.

---
+++ src/analizadorLexico/AnalizadorLexico.java
@@ -0,0 +1,19 @@
+package analizadorLexico;
+
+public class AnalizadorLexico {
+
+    private String buffer;
+
+    public AnalizadorLexico() {
+
+    }
+
+    public String getBuffer() {
+        return buffer;
+    }
+
+    public void setBuffer(String buffer) {
+        this.buffer = buffer;
+    }
+
+}

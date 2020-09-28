2c52f9907d7169bd5918b18b380c0d6c4daeb586
Nico Laurenti <nicolaurenti.94@gmail.com>
2020-09-27 22:40:36 -0300

    Iniciaizar Matriz de Acciones Semanticas

--- src/analizadorLexico/matrices/MatrizAccionesSemanticas.java
+++ src/analizadorLexico/matrices/MatrizAccionesSemanticas.java
@@ -18,4 +18,13 @@ public class MatrizAccionesSemanticas {
         matriz[estadoOrigen][simbolo] = as;
     }
 
+    //NICO
+    public void set(AccionSemantica[][] mas){
+        for(int i=0; i<=25; i++)
+            for(int j=0; j<=12; j++){
+                set(i,j,mas[i][j]);
+            }
+    }
 }
+
+

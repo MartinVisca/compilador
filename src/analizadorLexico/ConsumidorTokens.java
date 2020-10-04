7a3a4da1a1ad6301d429b34432fa4c1741281899
Martin Ignacio Visca <mvisca@conicet.gov.ar@18-037.devel.conicet.gov.ar>
2020-10-03 22:49:37 -0300

    Cambios en token y su consumidor.

--- src/analizadorLexico/ConsumidorTokens.java
+++ src/analizadorLexico/ConsumidorTokens.java
@@ -1,23 +1,36 @@
 package analizadorLexico;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Vector;
 
 public class ConsumidorTokens {
 
     public static void main(String[] args) {
-        String entrada = "";
-        AnalizadorLexico aLexico = new AnalizadorLexico(entrada);
-        Vector<Token> tokens = aLexico.getTokens();
-        for (Token t:tokens) {
+        //Logica de carga de archivo
+        /*try {
+            String path = "";
+            String stringArchivo = Files.readString(Paths.get(path));
+        } catch(IOException e) {}*/
+
+        String entrada = "hola; /n hOla$"; //Aca iria el string correspondiente al archivo
+        AnalizadorLexico analizadorLexico = new AnalizadorLexico(entrada);
+        Vector<Token> tokens = analizadorLexico.getTokens();
+        for (Token token : tokens) {
             System.out.println("----------------");
-            System.out.println("Tipo token: " + t.getTipo());
-            System.out.println("Lexema: " + t.getLexema());
-            RegistroSimbolo regSim = t.getRegistro();
-            if (regSim != null) {
-                System.out.println("Apunta a lexema: " + regSim.getLexema());
-                System.out.println("Total de apariciones: " + regSim.getTotal());
+            System.out.println("Tipo token: " + token.getTipo());
+            System.out.println("Lexema: " + token.getLexema());
+            RegistroSimbolo registroSimbolo = token.getRegistro();
+            if (registroSimbolo != null) {
+                System.out.println("Apunta a lexema: " + registroSimbolo.getLexema());
+                System.out.println("Total de apariciones: " + registroSimbolo.getTotal());
             }
         }
+
+        System.out.println("----------------");
+        analizadorLexico.imprimirErrores();
     }
 
 }

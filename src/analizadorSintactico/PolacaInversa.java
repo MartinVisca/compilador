9fbd0ed6df5a3b21d9989437b995780364dac8d2
Martin Ignacio Visca <mvisca@conicet.gov.ar@18-037.devel.conicet.gov.ar>
2020-11-12 19:11:50 -0300

    Estructura de Polaca Inversa agregada.

---
+++ src/analizadorSintactico/PolacaInversa.java
@@ -0,0 +1,30 @@
+package analizadorSintactico;
+
+import java.util.Vector;
+
+public class PolacaInversa {
+    //Estructura para almacenar la representación de código "Polaca Inversa".
+
+    private Vector<String> estructuraPolaca;
+
+    public PolacaInversa() {
+        this.estructuraPolaca = new Vector<>();
+    }
+
+    public String getElemento(int posicion) {
+        return this.estructuraPolaca.elementAt(posicion);
+    }
+
+    public void addElemento(String elemento) {
+        this.estructuraPolaca.add(elemento);
+    }
+
+    public void addElementoEnPosicion(String elemento, int posicion) {
+        this.estructuraPolaca.add(posicion, elemento);
+    }
+
+    public void vaciarPolaca() {
+        this.estructuraPolaca.removeAllElements();
+    }
+
+}
